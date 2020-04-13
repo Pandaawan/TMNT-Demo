2ca2f83695615f6160a43098484db8aebb23ba84
Pandaawan <jarrod.ocallaghan@gmail.com>
2020-04-13 22:30:44 +1000

    Scene fades, context clue, chest added. Architecture improved

---
+++ Assets/Scripts/Objects/RoomMove.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RoomMove : MonoBehaviour
+{
+    public Vector2 cameraChange;
+    //public Vector2 cameraMinChange;
+    //public Vector2 cameraMaxChange;
+
+    public Vector3 playerChange;
+    private CameraMovement cam;
+    public bool needText;
+    public string placeName;
+    public GameObject text;
+    public Text placeText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam = Camera.main.GetComponent<CameraMovement>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player") && !collision.isTrigger) //Check Player AND NOT collision/knock back box
+        {
+            cam.minPosition += cameraChange; //can use cameraMinChange for different sized rooms than default 25x25
+            cam.maxPosition += cameraChange; //as above but cameraMaxChange
+            collision.transform.position += playerChange;
+            if (needText)
+            {
+                StartCoroutine(placeNameCo());
+            }
+        }
+    }
+
+    private IEnumerator placeNameCo()
+    {
+        text.SetActive(true);
+        placeText.text = placeName;
+        yield return new WaitForSeconds(4f);
+        text.SetActive(false);
+    }
+}
