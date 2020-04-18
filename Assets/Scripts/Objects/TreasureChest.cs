9408043d9ae5ff87c071d5d2d560a09c87be1a8f
Pandaawan <jarrod.ocallaghan@gmail.com>
2020-04-18 16:24:26 +1000

    Added treasure chest contents/animation, patrolling enemy

--- Assets/Scripts/Objects/TreasureChest.cs
+++ Assets/Scripts/Objects/TreasureChest.cs
@@ -1,18 +1,89 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TreasureChest : Interactable
 {
+    public Item contents;
+    public Inventory playerInventory;
+    public bool isOpen;
+    public Signals raiseItem;
+    public GameObject dialogBox; // Inherited from UnityEngine.UI
+    public Text dialogText;
+    public Animator anim;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space) && playerInRange)
+        {
+            if (!isOpen)
+            {
+                // Open  the chest
+                OpenChest();
+            }
+            else
+            {
+                // Chest is already open
+                ChestAlreadyOpen();
+            }
+        }
+    }
+
+    public void OpenChest()
+    {
+        // Dialog window on
+        dialogBox.SetActive(true);
+
+        // Dialog text = contents text
+        dialogText.text = contents.itemDescription;
+
+        // Add contents to inventory
+        playerInventory.AddItem(contents);
+        playerInventory.currentItem = contents;
+
+        // Raise the signal to player to animate
+        raiseItem.Raise();
+
+        // Raise the context clue
+        context.Raise();
+
+        // Set chest to opened
+        isOpen = true;
+        anim.SetBool("Opened", true);
+    }
+
+    public void ChestAlreadyOpen()
+    {
+        // Dialog off
+        dialogBox.SetActive(false);
+
+        // Raise signal to player to stop animating
+        raiseItem.Raise();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player") && !collision.isTrigger && !isOpen)
+        {
+            context.Raise();
+            playerInRange = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player") && !collision.isTrigger && !isOpen)
+        {
+            context.Raise();
+            playerInRange = false;
+        }
     }
 }
