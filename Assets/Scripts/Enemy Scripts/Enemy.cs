a2a59ba64347ee00ad1bb119c9f7d2d97a456c5b
Pandaawan <jarrod.ocallaghan@gmail.com>
2020-05-06 22:20:12 +1000

    Added dungeon area. Enemy death effect. Heart pickup and coin pickup. Coin UI and counter.

--- Assets/Scripts/Enemy Scripts/Enemy.cs
+++ Assets/Scripts/Enemy Scripts/Enemy.cs
@@ -18,6 +18,7 @@ public class Enemy : MonoBehaviour
     public string enemyName;
     public int baseAttack;
     public float moveSpeed;
+    public GameObject deathEffect;
 
     private void Awake()
     {
@@ -29,7 +30,17 @@ public class Enemy : MonoBehaviour
         health -= damage;
         if(health <= 0)
         {
+            DeathEffect();
             this.gameObject.SetActive(false);
+        }
+    }
+
+    private void DeathEffect()
+    {
+        if(deathEffect != null)
+        {
+            GameObject effect = Instantiate(deathEffect, transform.position, Quaternion.identity);
+            Destroy(effect, 1f);
         }
     }
     public void Knock(Rigidbody2D myRigidbody, float knockTime, float damage)
---
+++ Assets/Scripts/Objects/Heart.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Heart : PowerUp
+{
+    public FloatValue playerHealth;
+    public FloatValue heartContainers;
+    public float amountToIncrease;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Player") && !collision.isTrigger) //!trigger checks that it's the physics collider
+        {
+            playerHealth.RuntimeValue += amountToIncrease;
+            if(playerHealth.initialValue > heartContainers.RuntimeValue * 2f)
+            {
+                playerHealth.initialValue = heartContainers.RuntimeValue * 2f;
+            }
+            powerUpSignal.Raise();
+            Destroy(this.gameObject);
+        }
+    }
+}
